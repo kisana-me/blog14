f463ab8b44747aea53b38766a0f20e3eb799d685
kisana <yuto01200120yuto@gmail.com>
2024-02-18 16:08:12 +0900

    [ADD]websiteとしての設定

--- src/app/javascript/application.js
+++ src/app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', (event) => {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-3JW0EY2GYV', { page_location: event.detail.url });
+  gtag('event', 'page_view', {
+    page_location: event.detail.url,
+    send_to: 'G-3JW0EY2GYV',
+  });
+});
