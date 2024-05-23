0878a1e2e7173afd49f91da4c914d64ed9d97f30
kisana <yuto01200120yuto@gmail.com>
2024-05-24 01:24:51 +0900

    [FIX]session名。runtime表示消す

--- src/config/application.rb
+++ src/config/application.rb
@@ -15,6 +15,8 @@ module App
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.middleware.delete Rack::Runtime
+    config.session_store :cookie_store, key: 'b_ses'
 
     # Configuration for the application, engines, and railties goes here.
     #
