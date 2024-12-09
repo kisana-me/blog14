0e5623d72cc60fb622d9e957aafe55f428b4ee9b
kisana <yuto01200120yuto@gmail.com>
2024-12-09 13:00:15 +0900

    [ADD]アカウントとタグをnoindex,nofollownに

--- src/app/controllers/admin/inquiries_controller.rb
+++ src/app/controllers/admin/inquiries_controller.rb
@@ -1,9 +1,25 @@
 class Admin::InquiriesController < Admin::ApplicationController
   def index
-    @inquiries = Inquiry.all
+    @inquiries = Inquiry.order(created_at: :DESC)
   end
   def show
     @inquiry = Inquiry.find_by(aid: params[:aid])
   end
+  def update
+    @inquiry = Inquiry.find_by(aid: params[:aid])
+    if @inquiry.update(inquiry_params)
+      flash[:success] = '変更しました'
+      redirect_to admin_inquiry_path(@inquiry.aid)
+    else
+      flash.now[:danger] = '変更できませんでした'
+      render 'show'
+    end
+  end
   private
+  def inquiry_params
+    params.require(:inquiry).permit(
+      :memo,
+      :done
+    )
+  end
 end
