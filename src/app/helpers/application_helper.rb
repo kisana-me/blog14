2a823dbcbb61c22cd9f4ae1b5b1e6ca103eb770d
kisana <yuto01200120yuto@gmail.com>
2025-02-07 00:38:22 +0900

    [FIX]turbo-prefetchをmetaタグでfalseに。navに筆者とタグを復活

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -10,10 +10,10 @@ module ApplicationHelper
   def full_url(path)
     File.join(ENV['APP_URL'], path)
   end
-  def link_to(name = nil, options = nil, html_options = nil, &block)
-    html_options ||= {}
-    html_options[:data] ||= {}
-    html_options[:data][:turbo_prefetch] = false unless html_options[:data].key?(:turbo_prefetch)
-    super(name, options, html_options, &block)
-  end
+  # def link_to(name = nil, options = nil, html_options = nil, &block)
+  #   html_options ||= {}
+  #   html_options[:data] ||= {}
+  #   html_options[:data][:turbo_prefetch] = false unless html_options[:data].key?(:turbo_prefetch)
+  #   super(name, options, html_options, &block)
+  # end
 end
